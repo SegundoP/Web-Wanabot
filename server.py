7b2f691c36d3cb08283b3d4138423e25ddd4c1db
Segundo <segundo.pueyrredon@wanabot.com>
2023-01-09 15:46:22 -0300

    version 09.01

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, session
 from flask import render_template
 from flask_babel import Babel, get_locale, lazy_gettext
+import smtplib
+import requests
+
+OWN_EMAIL = "pythontest@gmail.com"
+OWN_PASSWORD = "pythonudemy"
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -24,9 +29,45 @@ def translate():
     return lazy_gettext('Hello World')
 
 
-@app.route('/contact.html')
+# @app.route('/contact.html')
+# def contact():
+#     return render_template("contact.html")
+
+# @app.route('/contact.html', methods=["POST"])
+# def contact():
+#     data = request.form
+#     send_email(data["name"], data["email"], data["phone"], data["message"])
+#     return render_template("contact.html", msg_sent=True)
+
+@app.route("/contact.html", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, "segundo.pueyrredon@wanabot.com", email_message)
+
+
+
+# @app.route("/contact.html", methods=["GET", "POST"])
+# def contact():
+#     if request.method == "POST":
+#         data = request.form
+#         print(data["name"])
+#         print(data["email"])
+#         print(data["subject"])
+#         print(data["message"])
+#         return render_template("contact.html", msg_sent=True)
+#     return render_template("contact.html", msg_sent=False)
+
 
 
 @app.route('/project.html')
